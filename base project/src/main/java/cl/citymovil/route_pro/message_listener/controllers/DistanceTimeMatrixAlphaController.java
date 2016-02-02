86cebcce2191f87c9f60401b10074efad9c0a504
kpeet <kpeet@inf.utfsm.cl>
2016-02-02 12:09:13 -0300

    Limpieza del Controller

--- base project/src/main/java/cl/citymovil/route_pro/message_listener/controllers/DistanceTimeMatrixAlphaController.java
+++ base project/src/main/java/cl/citymovil/route_pro/message_listener/controllers/DistanceTimeMatrixAlphaController.java
@@ -3,7 +3,6 @@ package cl.citymovil.route_pro.message_listener.controllers;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -17,10 +16,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import cl.citymovil.route_pro.message_listener.dao.LocationDAO;
 import cl.citymovil.route_pro.message_listener.domain.DistanceTimeData;
-import cl.citymovil.route_pro.message_listener.domain.DistanceTimeDataComplete;
 import cl.citymovil.route_pro.message_listener.domain.Location;
 import cl.citymovil.route_pro.message_listener.services.DistanceTimeMatrixServiceAlpha;
-import cl.citymovil.route_pro.solver.util.LocationContainer;
 import cl.citymovil.route_pro.solver.util.LocationContainerForGoogleAsk;
 import cl.citymovil.route_pro.solver.util.RelationLocation;
 
@@ -44,7 +41,7 @@ public class DistanceTimeMatrixAlphaController {
     @RequestMapping(method = RequestMethod.GET)
      public void onSubmit()
      {
-    	
+    	logger.info("[(onSubmit)]start onSubmit");
     	   /********** Adicional*/
     	ArrayList<Location> arrayWithIdLocation = locationDAO.getLocationList();
     	  /********** Fin Adicional*/
@@ -54,73 +51,35 @@ public class DistanceTimeMatrixAlphaController {
     	
     	if(distanceTimeMatrixHashMap==null){
     		
-     		logger.info("(FAIL)Don't have Location.");
+     		logger.trace("[(onSubmit)](FAIL)Don't have Location.");
      		
      	}else{
      		
-     		logger.info("(OK)The PreprocessAlpha was return OK.");
-     		for (Map.Entry<Long, Map<Long, DistanceTimeData>> outer : distanceTimeMatrixHashMap.entrySet()) {
-    		    System.out.println("Key: " + outer.getKey() +  "\n");
-    		    
-    	
-			
-			for( Entry<Long, DistanceTimeData> algo : outer.getValue().entrySet() ){
-    			   System.out.println("Key = " + algo.getKey() + ", distance = " + algo.getValue().distance  );
-    		   }
-//    		    for (Map.Entry<Long, DistanceTimeDataComplete> inner : entry.getValue().entrySet()) {
-//    		        System.out.println("Key = " + inner.getKey() + ", Value = " + inner.getValue());
-//    		    }
-    		 } 
+     		logger.trace("[(onSubmit)](OK)The PreprocessAlpha was return OK.");
+//     		for (Map.Entry<Long, Map<Long, DistanceTimeData>> outer : distanceTimeMatrixHashMap.entrySet()) {
+//    		    System.out.println("Key: " + outer.getKey() +  "\n");
+//			for( Entry<Long, DistanceTimeData> algo : outer.getValue().entrySet() ){
+//    			   System.out.println("Key = " + algo.getKey() + ", distance = " + algo.getValue().distance  );
+//    		   }
+//    		 } 
      		ArrayList<LocationContainerForGoogleAsk> listOfLocationContainerForGoogle =new  ArrayList <LocationContainerForGoogleAsk>();
      		listOfLocationContainerForGoogle=distanceTimeMatrixServiceAlpha.PreprocessBeta(distanceTimeMatrixHashMap,arrayWithIdLocation);
      		if(listOfLocationContainerForGoogle==null){
-     			System.out.println("Location Container NO CONTINE NADA");
+     			logger.trace("[(onSubmit)](x)listOfLocationContainerForGoogle==null");
      		}else{
-     			System.out.println("ESTAMOS OK Location Container  CONTINE ALGO");
+     			logger.trace("[(onSubmit)](ok)listOfLocationContainerForGoogle!=null");
      		
-     		System.out.println("\n***********PUNTOS EN CONFLICTO****************\n");
-     		for(LocationContainerForGoogleAsk b: listOfLocationContainerForGoogle){
-     			System.out.println("\n ///////// (INICIO)Imprimiendo los resultados en un for ");
-//     			ArrayList<Location> listOrigin = b.getLocationOrigin();
-//     			ArrayList<Location> listDestiny = b.getLocationDestiny();
-//     			for(Location ori : listOrigin){
-//     				System.out.println("Datos Origen:"+ori.getLocationId());
-//     			}
-//     			for(Location dest : listDestiny){
-//     				System.out.println("Datos destino:"+dest.getLocationId());
-//     			}
-     			
-     			System.out.println("///////// Enviando a Process");
-     			
-     			ArrayList<RelationLocation> distanceMatrixList = distanceTimeMatrixServiceAlpha.Process(b);
-//     			
-     			distanceTimeMatrixServiceAlpha.PostProcessAlpha(distanceMatrixList);
+     			logger.trace("\n[(onSubmit)]***********PUNTOS EN CONFLICTO****************\n");
+	     		for(LocationContainerForGoogleAsk b: listOfLocationContainerForGoogle){
+	     			logger.trace("[(onSubmit)]Enviando a Process");
+	     			ArrayList<RelationLocation> distanceMatrixList = distanceTimeMatrixServiceAlpha.Process(b);
+	     			logger.trace("[(onSubmit)]Enviando a PostProcessAlpha");
+	     			distanceTimeMatrixServiceAlpha.PostProcessAlpha(distanceMatrixList);
+	     		}
      		}
-     		}
-     		
-     		
-     		
-     		
-     		
-//     		for(int count=0; count < locationContainerForGoogle.size(); count++){
-////     			ArrayList<RelationLocation>  distanceMatrixList = distanceTimeMatrixServiceAlpha.Process(locationContainerForGoogle.get(count));
-//     		}
-//     			
- 		   	
- 		  // 	distanceMatrixService.PostProcessAlpha(distanceMatrixList);
-    	
      	} 
      }
     protected void formBackingObject(HttpServletRequest request) throws ServletException {
     	
     }
-
-//    public void setLocationManager(DistanceMatrixService distanceMatrixService) {
-//        this.distanceMatrixService = distanceMatrixService;
-//    }
-//
-//    public DistanceMatrixService getProductManager() {
-//        return distanceMatrixService;
-//    } 
-
 }
